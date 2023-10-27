1e02da01033d6296845dacdbe3121b169373972f
Shivam Mishra <123864184+Heroshi1947@users.noreply.github.com>
2023-10-27 13:20:17 +0530

    Update Login.js

--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
   // this will connect frontend with backend to actually create an user by sending data stored in body to database
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:4000/api/loginuser", {
+    const response = await fetch("/api/loginuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

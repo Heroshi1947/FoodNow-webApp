cc5f508365b1ea1fef7f22f85419ffb924952db2
Shivam Mishra <123864184+Heroshi1947@users.noreply.github.com>
2023-10-28 15:21:01 +0530

    Update Home.js

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
   const [search, setSearch] = useState("");
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:4000/api/foodData", {
+    let response = await fetch("/api/foodData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
